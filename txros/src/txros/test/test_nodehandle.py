57921fe91713bc3cbdf7e28da1423f43fca0a206
Forrest Voight <forrest@forre.st>
2016-03-12 08:04:24 +0000

    Add NodeHandle param test

--- txros/src/txros/test/test_nodehandle.py
+++ txros/src/txros/test/test_nodehandle.py
@@ -11,3 +11,18 @@ class Test(unittest.TestCase):
     @defer.inlineCallbacks
     def test_creation(self):
         yield test_util.call_with_nodehandle(lambda nh: defer.succeed(None))
+    
+    @defer.inlineCallbacks
+    def test_params(self):
+        @defer.inlineCallbacks
+        def f(nh):
+            k = '/my_param'
+            v = ['hi', 2]
+            
+            assert not (yield nh.has_param(k))
+            yield nh.set_param(k, v)
+            assert (yield nh.has_param(k))
+            assert (yield nh.get_param(k)) == v
+            yield nh.delete_param(k)
+            assert not (yield nh.has_param(k))
+        yield test_util.call_with_nodehandle(f)
