6026bfdc5038550a1aa85faf7dbe14e6deddb1ba
Cameron Brown <me@cbrxyz.com>
2022-03-03 13:34:27 -0500

    Update txros publisher to py3

--- txros/src/txros/publisher.py
+++ txros/src/txros/publisher.py
@@ -8,8 +8,7 @@ from twisted.internet import defer, error
 from txros import util, tcpros
 
 
-class Publisher(object):
-
+class Publisher:
     def __init__(self, node_handle, name, message_type, latching=False):
         self._node_handle = node_handle
         self._name = self._node_handle.resolve_name(name)
@@ -26,15 +25,25 @@ class Publisher(object):
     @util.cancellableInlineCallbacks
     def _think(self):
         try:
-            assert ('topic', self._name) not in self._node_handle._tcpros_handlers
-            self._node_handle._tcpros_handlers['topic', self._name] = self._handle_tcpros_conn
-            assert ('requestTopic', self._name) not in self._node_handle._xmlrpc_handlers
-            self._node_handle._xmlrpc_handlers['requestTopic', self._name] = self._handle_requestTopic
+            assert ("topic", self._name) not in self._node_handle._tcpros_handlers
+            self._node_handle._tcpros_handlers[
+                "topic", self._name
+            ] = self._handle_tcpros_conn
+            assert (
+                "requestTopic",
+                self._name,
+            ) not in self._node_handle._xmlrpc_handlers
+            self._node_handle._xmlrpc_handlers[
+                "requestTopic", self._name
+            ] = self._handle_requestTopic
             try:
                 while True:
                     try:
                         yield self._node_handle._master_proxy.registerPublisher(
-                            self._name, self._type._type, self._node_handle._xmlrpc_server_uri)
+                            self._name,
+                            self._type._type,
+                            self._node_handle._xmlrpc_server_uri,
+                        )
                     except Exception:
                         traceback.print_exc()
                     else:
@@ -43,11 +52,12 @@ class Publisher(object):
             finally:
                 try:
                     yield self._node_handle._master_proxy.unregisterPublisher(
-                        self._name, self._node_handle._xmlrpc_server_uri)
+                        self._name, self._node_handle._xmlrpc_server_uri
+                    )
                 except Exception:
                     traceback.print_exc()
-                del self._node_handle._tcpros_handlers['topic', self._name]
-                del self._node_handle._xmlrpc_handlers['requestTopic', self._name]
+                del self._node_handle._tcpros_handlers["topic", self._name]
+                del self._node_handle._xmlrpc_handlers["requestTopic", self._name]
         finally:
             self._shutdown_finished.callback(None)
 
@@ -58,29 +68,40 @@ class Publisher(object):
         return util.branch_deferred(self._shutdown_finished)
 
     def _handle_requestTopic(self, protocols):
-        return 1, 'ready on ' + self._node_handle._tcpros_server_uri, [
-            'TCPROS', self._node_handle._tcpros_server_addr[0], self._node_handle._tcpros_server_addr[1]]
+        return (
+            1,
+            "ready on " + self._node_handle._tcpros_server_uri,
+            [
+                "TCPROS",
+                self._node_handle._tcpros_server_addr[0],
+                self._node_handle._tcpros_server_addr[1],
+            ],
+        )
 
     @util.cancellableInlineCallbacks
     def _handle_tcpros_conn(self, headers, conn):
         try:
             # XXX handle headers
 
-            conn.sendString(tcpros.serialize_dict(dict(
-                callerid=self._node_handle._name,
-                type=self._type._type,
-                md5sum=self._type._md5sum,
-                latching='1' if self._latching else '0',
-            )))
+            conn.sendString(
+                tcpros.serialize_dict(
+                    dict(
+                        callerid=self._node_handle._name,
+                        type=self._type._type,
+                        md5sum=self._type._md5sum,
+                        latching="1" if self._latching else "0",
+                    )
+                )
+            )
 
             if self._latching and self._last_message_data is not None:
                 conn.sendString(self._last_message_data)
 
-            self._connections[conn] = headers['callerid']
+            self._connections[conn] = headers["callerid"]
             try:
                 while True:
                     x = yield conn.receiveString()
-                    print repr(x)
+                    print(repr(x))
             finally:
                 del self._connections[conn]
         except (error.ConnectionDone, error.ConnectionLost):
@@ -100,4 +121,4 @@ class Publisher(object):
             self._last_message_data = data
 
     def get_connections(self):
-        return list(self._connections.itervalues())
+        return list(self._connections.values())
